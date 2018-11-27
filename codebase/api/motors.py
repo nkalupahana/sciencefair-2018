3dd0ac1d4fd8dd0771e09ae4bd13b9ca6f373a30
nkalupahana <avishkank@gmail.com>
2018-11-26 22:33:27 -0800

    Bug fixes

--- codebase/api/motors.py
+++ codebase/api/motors.py
@@ -1,4 +1,5 @@
 def turnOffMotors():
+    mh = Adafruit_MotorHAT(addr=0x60)
     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
