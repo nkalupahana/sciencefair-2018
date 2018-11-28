2f2b5e00dafaa64811f2f58b73909fd0c20cee92
nkalupahana <avishkank@gmail.com>
2018-11-27 20:24:43 -0800

    Fixed some API issues

--- codebase/api/motors.py
+++ codebase/api/motors.py
@@ -7,7 +7,7 @@ def turnOffMotors():
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
 
 class Cutter:
-    def __init__(motor):
+    def __init__(self, motor):
         self.cutter = motor
 
     def cut():
@@ -19,7 +19,7 @@ class Cutter:
 
 
 class DriveSystem:
-    def __init__(motor1, motor2):
+    def __init__(self, motor1, motor2):
         self.m1 = motor1
         self.m2 = motor2
 
