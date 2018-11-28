51a6466f4bd8af54cb04fe6a5983c1138ad544b1
nkalupahana <avishkank@gmail.com>
2018-11-27 20:23:18 -0800

    Switched to cutter API

--- codebase/cutter_test.py
+++ codebase/cutter_test.py
@@ -3,13 +3,9 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 from time import sleep
 import atexit
+from api.motors import *
 
-# create a default object, no changes to I2C address or frequency
+atexit.register(turnOffMotors)
 mh = Adafruit_MotorHAT(addr=0x60)
-motor = mh.getMotor(3)
-
-motor.setSpeed(255)
-motor.run(Adafruit_MotorHAT.FORWARD)
-sleep(4)
-motor.setSpeed(0)
-motor.run(Adafruit_MotorHAT.RELEASE)
+cutter = Cutter(mh.getMotor(3))
+cutter.cut()
