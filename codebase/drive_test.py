0b53a59ec4289ffb9cc015f02d20e457a3e3280f
nkalupahana <avishkank@gmail.com>
2018-11-29 22:22:48 -0800

    More work on drive system

--- codebase/drive_test.py
+++ codebase/drive_test.py
@@ -8,7 +8,7 @@ from api.motors import *
 atexit.register(turnOffMotors)
 mh = Adafruit_MotorHAT(addr=0x60)
 ds = DriveSystem(mh.getMotor(1), mh.getMotor(2))
-ds.start(50)
+ds.start(100)
 sleep(3)
 ds.stop()
 
