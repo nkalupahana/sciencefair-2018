23a6f112ba58365688bfb23144c50f820613c06d
nkalupahana <avishkank@gmail.com>
2019-01-04 19:21:13 -0800

    Py3 fixes

--- codebase/examples/position/loc.py
+++ codebase/examples/position/loc.py
@@ -13,7 +13,7 @@ class GpsPoller(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
     global gpsd #bring it in scope
-    gpsd = gps.GPS(mode=WATCH_ENABLE) #starting the stream of info
+    gpsd = gps.GPS(mode=gps.WATCH_ENABLE) #starting the stream of info
     self.current_value = None
     self.running = True #setting the thread running to true
 
