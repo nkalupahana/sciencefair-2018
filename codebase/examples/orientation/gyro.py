ca77f41cf4449d216527415f2eeb60d3db1b84b2
nkalupahana <avishkank@gmail.com>
2019-01-15 23:09:20 -0800

    Prepare for recalibration

--- codebase/examples/orientation/gyro.py
+++ codebase/examples/orientation/gyro.py
@@ -8,7 +8,7 @@ from api.ninedof import *
 
 orient = NineDOF()
 q = Queue()
-orient.gyro_heading_begin_tracking(0.05, q)
+orient.gyro_heading_begin_tracking(0.04, q)
 
 while True:
     print(q.get())
