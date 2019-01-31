d3d0da96c7988f8d2ecb732629b74e8ee35baf48
nkalupahana <avishkank@gmail.com>
2019-01-31 13:03:51 -0800

    Minor fixes

--- codebase/api/ninedof.py
+++ codebase/api/ninedof.py
@@ -139,7 +139,7 @@ class NineDOF:
 
             forceMag = abs(adata["x"] * 1000) + abs(adata["y"] * 1000) + abs(adata["z"] * 1000)
             if forceMag > 8192 and forceMag < 32768:
-                zaccelturn = atan2((adata["x"] - (ACCEL_OFFSETS[0] * 1000)), (adata["y"] - (ACCEL_OFFSETS[1] * 1000))) * (180 / pi)
+                zaccelturn = atan2(adata["x"], adata["y"]) * (180 / pi)
                 hold = hold * 0.98 + zaccelturn * 0.02
             else:
                 print("REJECTED " + str(forceMag))
