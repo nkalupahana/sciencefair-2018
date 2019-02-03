4ed083cd49f5e8f1ea80f004e78b5d7eec109154
nkalupahana <avishkank@gmail.com>
2019-02-03 13:35:39 -0800

    Switched savetest to queue

--- codebase/api/positioning.py
+++ codebase/api/positioning.py
@@ -22,4 +22,4 @@ class Positioning:
         while True:
             gpsd.next()
             sleep(1)
-            q.put({"lat": gpsd.fix.latitude, "lng": gpsd.fix.latitude})
+            q.put({"lat": round(gpsd.fix.latitude, 5), "lng": round(gpsd.fix.latitude, 5)})
--- codebase/examples/position/savetest.py
+++ codebase/examples/position/savetest.py
@@ -3,21 +3,26 @@ import sys
 sys.path.append("../../")
 sys.path.append("../../api")
 
-
 from api.globals import *
 from api.positioning import *
 from api.database import *
 from api.button import *
+from multiprocessing import Queue
 
 # Create database & GPS objects
 db = Database(DATABASE_NAME)
 db.prepare()
-position = Positioning()
+
+q = Queue()
+position = Positioning(q)
 
 # Create function to save state
 def saveState():
+    with q.mutex:
+        q.queue.clear()
+
     print("SAVING")
-    pos = position.getLatLng()
+    pos = q.get()
     print(pos)
     db.put(pos["lat"], pos["lng"])
 
