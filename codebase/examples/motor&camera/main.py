bf32f35e8a614ea02a09cc04f489b4123544154f
nkalupahana <avishkank@gmail.com>
2019-02-03 19:47:35 -0800

    Preparation for camera test

--- codebase/examples/motor&camera/main.py
+++ codebase/examples/motor&camera/main.py
@@ -18,20 +18,14 @@ ds = DriveSystem(mh.getMotor(BASE_MOTOR_1), mh.getMotor(BASE_MOTOR_2))
 cutter = Cutter(mh.getMotor(CUTTER_MOTOR))
 camera = Camera(GRAPH_PATH, DETECTION_LIMIT, IOU_LIMIT, LABELS)
 
-# Start main driver system thread
-drivethread = multiprocessing.Process(target=_driver)
-drivethread.daemon = True
-drivethread.start()
+ds.go(100)
 
-def _driver():
-    ds.go(100)
-
-    while True:
-        ymin = camera.run()
-        if (camera.run() != 0):
-            sleep(ymin / 40)
-            ds.go(50)
-            cutter.cut()
-            ds.go(-10) # Wait for further camera input
-        else:
-            ds.go(100)
+while True:
+    ymin = camera.run()
+    if (camera.run() != 0):
+        sleep(ymin / 40)
+        ds.go(50)
+        cutter.cut()
+        ds.go(-10) # Wait for further camera input
+    else:
+        ds.go(100)
