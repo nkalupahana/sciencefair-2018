9ec3804d92a6d9aa1769ed15ebbeeb0898bce01e
nkalupahana <avishkank@gmail.com>
2019-02-10 15:15:38 -0800

    Turn fixes

--- codebase/examples/gdrive/turn.py
+++ codebase/examples/gdrive/turn.py
@@ -7,7 +7,9 @@ from api.globals import *
 from time import sleep
 
 gdrive = GyroDrive(BASE_MOTOR_1, BASE_MOTOR_2)
-gdrive.ga_turn(-60)
+gdrive.ga_turn(-62)
+gdrive.ga_turn(62)
+
 #sleep(5)
 #gdrive.ga_turn(90)
 #gdrive.c_turn(90)
