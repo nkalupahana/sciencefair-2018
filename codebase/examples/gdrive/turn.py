2b9b6b22529b63def150be193e259480044c9820
nkalupahana <avishkank@gmail.com>
2019-02-10 15:04:07 -0800

    Added globals and path import fix

--- codebase/examples/gdrive/turn.py
+++ codebase/examples/gdrive/turn.py
@@ -1,10 +1,12 @@
 import sys
 sys.path.append("../../")
+sys.path.append("../../api")
 
 from api.gyrodrive import *
+from api.globals import *
 from time import sleep
 
-gdrive = GyroDrive()
+gdrive = GyroDrive(BASE_MOTOR_1, BASE_MOTOR_2)
 gdrive.ga_turn(90)
 #sleep(5)
 #gdrive.ga_turn(90)
