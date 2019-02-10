c248508b79356892f0fa7886412c7a7d6f6eae70
nkalupahana <avishkank@gmail.com>
2019-02-10 15:58:30 -0800

    Globals fixes

--- codebase/examples/gdrive/straightdrive.py
+++ codebase/examples/gdrive/straightdrive.py
@@ -3,9 +3,10 @@ sys.path.append("../../")
 sys.path.append("../../api")
 
 from api.gyrodrive import *
+from api.globals import *
 from time import sleep
 
-gdrive = GyroDrive()
+gdrive = GyroDrive(BASE_MOTOR_1, BASE_MOTOR_2)
 gdrive.straight_drive_start(100)
 sleep(15)
 gdrive.straight_drive_terminate()
