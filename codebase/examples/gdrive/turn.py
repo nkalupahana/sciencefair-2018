f32852b161c943bdfbf0a609ed5c1a7edfb81bcd
nkalupahana <avishkank@gmail.com>
2019-02-10 15:12:48 -0800

    Angle switching

--- codebase/examples/gdrive/turn.py
+++ codebase/examples/gdrive/turn.py
@@ -7,7 +7,7 @@ from api.globals import *
 from time import sleep
 
 gdrive = GyroDrive(BASE_MOTOR_1, BASE_MOTOR_2)
-gdrive.ga_turn(65)
+gdrive.ga_turn(-60)
 #sleep(5)
 #gdrive.ga_turn(90)
 #gdrive.c_turn(90)
