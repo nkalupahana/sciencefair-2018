02c2f62b8f2048f03eeaaeb8eb17e430ca286200
nkalupahana <avishkank@gmail.com>
2019-02-10 14:09:04 -0800

    Count not working -- ref set to getPoints

--- codebase/main_addpoints.py
+++ codebase/main_addpoints.py
@@ -13,21 +13,18 @@ from multiprocessing import Queue
 db = Database(DATABASE_NAME)
 db.prepare()
 
-count = 0
-
 # Create function to save state
 def saveState():
-    global count
     print("SAVING")
     pos = getLatLng()
     print(pos)
     db.put(pos["lat"], pos["lng"])
-    count += 1
 
 # Create button, activate
 button = ButtonActionThread(BUTTON_PIN, saveState, CONFIRM_PITCH) # TODO: check pitch
 button.activate()
 
 # stay alive
-while count < 4:
+while db.getPoints().rowcount < 4:
+    print(db.getPoints().rowcount)
     sleep(1)
