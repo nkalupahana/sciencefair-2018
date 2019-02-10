7a941cf5ed362159251b20f3cb61dd0bd43ddb9c
nkalupahana <avishkank@gmail.com>
2019-02-10 15:34:40 -0800

    Adjusted angle

--- codebase/examples/gdrive/turn.py
+++ codebase/examples/gdrive/turn.py
@@ -7,9 +7,9 @@ from api.globals import *
 from time import sleep
 
 gdrive = GyroDrive(BASE_MOTOR_1, BASE_MOTOR_2)
-gdrive.ga_turn(-50)
-sleep(5)
-gdrive.ga_turn(50)
+gdrive.ga_turn(-53)
+sleep(1)
+gdrive.ga_turn(53)
 
 #sleep(5)
 #gdrive.ga_turn(90)
