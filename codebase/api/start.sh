bdd23fb9715bee38bd35b0fcbb9712d2d329fd92
nkalupahana <avishkank@gmail.com>
2019-02-10 18:06:03 -0800

    Quick fixes

--- codebase/api/start.sh
+++
@@ -1,6 +0,0 @@
-/root/sciencefair-2018/codebase/api/tonecreation 60 0.4
-/root/sciencefair-2018/codebase/api/tonecreation 64 0.2
-/root/sciencefair-2018/codebase/api/tonecreation 67 0.2
-/root/sciencefair-2018/codebase/api/tonecreation 64 0.2
-/root/sciencefair-2018/codebase/api/tonecreation 67 0.2
-/root/sciencefair-2018/codebase/api/tonecreation 72 0.75
