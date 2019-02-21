d0abfc549d4741c99ffc144ce657d0192d6589c1
nkalupahana <avishkank@gmail.com>
2019-02-20 20:40:39 -0800

    Style and docs for lower-level motor control classes

--- codebase/api/motors.py
+++ codebase/api/motors.py
@@ -1,6 +1,7 @@
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 from time import sleep
 
+# Function to be passed into atexit to stop motors on unclean program exit
 def turnOffMotors():
     mh = Adafruit_MotorHAT(addr=0x60)
     mh.getMotor(1).setSpeed(0)
@@ -12,10 +13,12 @@ def turnOffMotors():
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
+# Cutter motor control
 class Cutter:
     def __init__(self, motor):
         self.cutter = motor
 
+    # Runs cutter
     def cut(self):
         self.cutter.setSpeed(255)
         self.cutter.run(Adafruit_MotorHAT.FORWARD)
@@ -23,15 +26,23 @@ class Cutter:
         self.cutter.setSpeed(0)
         self.cutter.run(Adafruit_MotorHAT.RELEASE)
 
-
+# Base drive control
 class DriveSystem:
     def __init__(self, motor1, motor2):
         self.m1 = motor1
         self.m2 = motor2
 
+    # Adjusts base motor speed (add to right, subtract from left)
     def adjustSpeed(self, adj):
-        self.m1.setSpeed(self.currentSpeed + adj)
+        # Speed adjustment for left motor
+        if (self.currentSpeed + adj) >= 0:
+            self.m1.run(Adafruit_MotorHAT.FORWARD)
+            self.m1.setSpeed(self.currentSpeed + adj)
+        else:
+            self.m1.run(Adafruit_MotorHAT.BACKWARD)
+            self.m1.setSpeed(abs(self.currentSpeed + adj))
 
+        # Speed adjustment for right motor
         if (self.currentSpeed - adj) >= 0:
             self.m2.run(Adafruit_MotorHAT.FORWARD)
             self.m2.setSpeed(self.currentSpeed - adj)
@@ -39,16 +50,19 @@ class DriveSystem:
             self.m2.run(Adafruit_MotorHAT.BACKWARD)
             self.m2.setSpeed(abs(self.currentSpeed - adj))
 
+    # Base drive base speed set function (no checks)
     def _setSpeed(self, speed):
         self.currentSpeed = speed
         self.m1.setSpeed(speed)
         self.m2.setSpeed(speed)
 
-    def go(self, speed, override = False):
+    # Base drive speed set function (with checks)
+    def go(self, speed, override=False):
         if speed == 0 and not override:
             raise ValueError('You shouldn\'t be setting speed to 0! Call' +
-            ' .stop() instead!')
+                             ' .stop() instead!')
 
+        # Set direction and speed
         if speed >= 0:
             self.m1.run(Adafruit_MotorHAT.FORWARD)
             self.m2.run(Adafruit_MotorHAT.FORWARD)
@@ -58,6 +72,7 @@ class DriveSystem:
             self.m2.run(Adafruit_MotorHAT.BACKWARD)
             self._setSpeed(-speed)
 
+    # Stop drive base motors
     def stop(self):
         self._setSpeed(0)
         self.m1.run(Adafruit_MotorHAT.RELEASE)
